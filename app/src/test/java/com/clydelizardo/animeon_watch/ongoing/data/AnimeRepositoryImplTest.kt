39db5b86b48608a17a79ca77b1a36b37b2d72256
Clyde Lizardo <clyde.lizardo@gmail.com>
2024-08-16 02:47:27 +0100

    Add test for AnimeRepositoryImpl

---
+++ app/src/test/java/com/clydelizardo/animeon_watch/ongoing/data/AnimeRepositoryImplTest.kt
@@ -0,0 +1,62 @@
+package com.clydelizardo.animeon_watch.ongoing.data
+
+import com.clydelizardo.animeon_watch.MainCoroutineRule
+import com.clydelizardo.domain.AnimeModel
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit4.MockKRule
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+import org.openapitools.network.apis.SeasonsApi
+import org.openapitools.network.models.Anime
+import org.openapitools.network.models.AnimeSearch
+import retrofit2.Response
+
+class AnimeRepositoryImplTest {
+    @get:Rule
+    val mockKRule = MockKRule(this)
+
+    @get:Rule
+    val mainCoroutineRule = MainCoroutineRule()
+
+    lateinit var repositoryImpl: AnimeRepositoryImpl
+
+    @MockK
+    lateinit var seasonsApi: SeasonsApi
+
+    @MockK
+    lateinit var mapper: AnimeToAnimeModelMapper
+
+    @Test
+    fun `Given API is available, When requesting details, Then mapped result is returned`() =
+        runTest {
+            val mockAnime = mockk<Anime>()
+            val successResponseBody = mockk<AnimeSearch> {
+                every { data } returns listOf(mockAnime)
+            }
+            val mockAnimeModel = mockk<AnimeModel>()
+            coEvery { seasonsApi.getSeasonNow() } returns Response.success(successResponseBody)
+            coEvery { mapper.map(mockAnime) } returns mockAnimeModel
+
+            repositoryImpl = AnimeRepositoryImpl(seasonsApi, mapper)
+            val result = repositoryImpl.getOngoingSeasonAnime()
+            Assert.assertEquals(Result.success(listOf(mockAnimeModel)), result)
+        }
+
+    @Test
+    fun `Given API is unavailable, When requesting details, Then failure result is returned`() =
+        runTest {
+            val failureResponse = mockk<Response<AnimeSearch>> {
+                every { isSuccessful } returns false
+            }
+            coEvery { seasonsApi.getSeasonNow() } returns failureResponse
+
+            repositoryImpl = AnimeRepositoryImpl(seasonsApi, mapper)
+            val result = repositoryImpl.getOngoingSeasonAnime()
+            Assert.assertTrue(result.isFailure)
+        }
+}
