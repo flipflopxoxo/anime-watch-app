084a64738500b7f14885e5b42d0b68fd0ddadae5
Clyde Lizardo <clyde.lizardo@sainsburys.co.uk>
2024-09-09 10:46:59 +0100

    Optimise imports

--- common/src/main/java/com/clydelizardo/common/TestCoroutinesRule.kt
+++ common/src/main/java/com/clydelizardo/common/TestCoroutinesRule.kt
@@ -2,12 +2,7 @@ package com.clydelizardo.common
 
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.TestCoroutineDispatcher
-import kotlinx.coroutines.test.TestCoroutineScope
-import kotlinx.coroutines.test.TestScope
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestWatcher
